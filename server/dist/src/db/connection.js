41f07e8724c9d3d5f37cd07a688ca213936ccaa7
FrancoPersonal <francothobokholt@gmail.com>
2025-07-05 18:09:12 -0300

    Conexión a PostgreSQL configurada

--- server/dist/src/db/connection.js
+++ server/dist/src/db/connection.js
@@ -1,8 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('administraccion_clientes', 'root', 'Thobokholt123', {
-    host: 'localhost',
-    dialect: 'mysql'
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: 'postgres',
+    logging: false,
 });
 exports.default = sequelize;
